8b5dda4bb383c4080ebbc59936ead0cf96fed18f
alok328 <40838760+alok328@users.noreply.github.com>
2021-02-06 11:45:27 +0530

    created spring app

---
+++ src/main/java/com/hackas/StudentDash/StudentDashApplication.java
@@ -0,0 +1,13 @@
+package com.hackas.StudentDash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackas/StudentDash/StudentDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackas.StudentDash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
